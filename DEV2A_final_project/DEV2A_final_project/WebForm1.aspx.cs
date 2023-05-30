bc43b3bf4edf00c9163a9f3fa1c3955929ef24f5
hlakudi200 <112486977+hlakudi200@users.noreply.github.com>
2023-05-30 10:10:31 +0200

    admin_changes

---
+++ DEV2A_final_project/DEV2A_final_project/WebForm1.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DEV2A_final_project
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        public void btn_NextClick(object sender, EventArgs e)
+        {
+            //Validaters
+          
+
+        }
+        public void btn_NewPaymentsClick(object sender, EventArgs e)
+        {
+            //validaters 
+           
+        }
+    }
+}
